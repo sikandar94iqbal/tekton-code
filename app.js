ba33eba75172041c19e2843175c95dff4e72d673
Sikandar Iqbal <sikandar.iqbal@ibm.com>
2020-05-12 11:18:27 +0500

    nice

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 
 app.get('/', (req, res) => {
-  res.send("Hello from Appsody Demo! to Hell0 fro Cloud Paks!!");
+  res.send("Hello from Appsody Demoo! to Hell0 from Cloud Paks!!");
 });
  
 module.exports.app = app;
